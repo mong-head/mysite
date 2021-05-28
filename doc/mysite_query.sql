5eb906d3e0c2d2fcb3738f2aa28753b4d852011b
mong_head <camara1melon315@gmail.com>
2021-05-28 19:01:21 +0900

    query

--- doc/mysite_query.sql
+++ doc/mysite_query.sql
@@ -7,5 +7,8 @@ insert into user values(null,'관리자','admin@mysite.com','1111','female');
 -- select
 select no,name,email,password,gender from user;
 
+-- update
+update user set name = '배유진', password='1111' , gender='female' where no = 2;
+
 -- login(select)
 select no,name from user where email="admin@mysite.com" and password="1111";
