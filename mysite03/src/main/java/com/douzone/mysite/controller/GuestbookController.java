1d2ab57cff2f1a6328687d6c5b73ff6bbcd2d105
mong_head <camara1melon315@gmail.com>
2021-06-04 09:44:04 +0900

    BEAN 설정 : ViewResolver 설정, 코드에 적용

--- mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/GuestbookController.java
@@ -24,7 +24,7 @@ public class GuestbookController {
 	public String index(Model model) {
 		List<GuestbookVo> list = guestbookService.getMessageList();
 		model.addAttribute("list",list);
-		return "/WEB-INF/views/guestbook/list.jsp";
+		return "guestbook/list";
 	}
 	
 	@RequestMapping(value="/add", method=RequestMethod.POST)
@@ -37,7 +37,7 @@ public class GuestbookController {
 	@RequestMapping(value="/delete/{no}", method=RequestMethod.GET)
 	public String delete(@PathVariable("no") Long no, Model model) {
 		model.addAttribute("no",no);
-		return "/WEB-INF/views/guestbook/deleteform.jsp";
+		return "guestbook/deleteform";
 	}
 	
 	//delete
--- mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -8,6 +8,6 @@ public class MainController {
 	
 	@RequestMapping("")
 	public String main() {
-		return "WEB-INF/views/main/index.jsp";
+		return "main/index";
 	}
 }
