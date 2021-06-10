4a7793e3aaaa62d6581f1ecaf87b7185cfd83c97
mong_head <camara1melon315@gmail.com>
2021-06-10 12:46:00 +0900

    RESTFUL coding

--- mysite03/src/main/java/com/douzone/mysite/controller/api/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/api/UserController.java
@@ -4,23 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.douzone.mysite.service.UserService;
 import com.douzone.mysite.vo.UserVo;
 
-@Controller("userControllerApi") //다른 패키지에 같은 이름의 controller가 있기에 HandlerMapping시 id가 두 컨트롤러가 똑같이 들어갈 수 있어서 id를 임의로 설정해줌
+//@ResponseBody 있는 handler만 있는 경우 RestController 사용
+@RestController("userControllerApi") //다른 패키지에 같은 이름의 controller가 있기에 HandlerMapping시 id가 두 컨트롤러가 똑같이 들어갈 수 있어서 id를 임의로 설정해줌
 @RequestMapping("/user/api")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
-	@ResponseBody
-	@RequestMapping("/checkemail")
+	//@ResponseBody
+	//@RequestMapping("/checkemail",method=RequestMethod.GET)
+	@GetMapping(value="/checkemail") //restful coding
 	public Object checkEmail(@RequestParam(value="email", required=true, defaultValue="") String email) {
 		
 		UserVo userVo = userService.getUser(email);
